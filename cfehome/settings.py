e8479f1a7d0d6b2104341070d3707588fc8c5af0
Joshijax <49172509+Joshijax@users.noreply.github.com>
2020-04-27 14:41:23 +0100

    Update settings.py

--- cfehome/settings.py
+++ cfehome/settings.py
@@ -84,19 +84,6 @@ DATABASES = {
     }
 }
 
-DATABASES = {
-'default': {
-'ENGINE': 'django.db.backends.mysql',
-'NAME': 'channel',
-'USER': 'root',
-'PASSWORD': '',
-'HOST': 'localhost',
-'PORT': '3306',
-'OPTIONS': {
-'init_command': "SET sql_mode = 'STRICT_TRANS_TABLES'"
-}
-}
-}
 
 
 
